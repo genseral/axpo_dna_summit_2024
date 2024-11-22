3c739d6989aeb3da14fde9a3cb2793b2970dddb3
guidooswaldDB <guidooswaldDB>
2024-11-22 13:17:25 +0000

    some dbdemos
    installed and modified to make them work (iOT still broken due to UDF not working in DLT)

--- guido/dbdemos install.py
+++ guido/dbdemos install.py
@@ -15,4 +15,16 @@ dbdemos.list_demos()
 
 # COMMAND ----------
 
+dbdemos.install('aibi-customer-support', use_current_cluster=True, overwrite=True, catalog='dbw_databricks_dna_hackathon_databricks_stream', schema='guido_oswald')
+
+# COMMAND ----------
+
 dbdemos.install('aibi-marketing-campaign', use_current_cluster=True, overwrite=True, catalog='dbw_databricks_dna_hackathon_databricks_stream', schema='guido_oswald')
+
+# COMMAND ----------
+
+dbdemos.install('lakehouse-fsi-fraud', use_current_cluster=True, overwrite=True, catalog='dbw_databricks_dna_hackathon_databricks_stream', schema='guido_oswald')
+
+# COMMAND ----------
+
+dbdemos.install('lakehouse-iot-platform', use_current_cluster=True, overwrite=True, catalog='dbw_databricks_dna_hackathon_databricks_stream', schema='guido_oswald')
